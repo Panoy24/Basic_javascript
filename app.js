0c21da7172c3bc28ba61728cdb539291638fc078
Soukphachan Sayakone <Soukphachan@tplus.la>
2024-05-03 16:51:32 +0700

    add app.js to github

---
+++ app.js
@@ -0,0 +1,11 @@
+//console.log("Hello, " + process.argv[2] + "!");
+/* var firstName = 'Soukphachan';
+var lastName = 'SAYAKONE';
+
+console.log(firstName + " " + lastName); 
+let className = "API Development by NodeJS";
+console.log(className); 
+*/
+const emp_id = 110;
+console.log("Employee ID: " + emp_id);
+
